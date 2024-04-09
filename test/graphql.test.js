6df655698f69577dd83e83fe732c17347cbd466e
Rani Shaar <rani.shaar@123loadboard.com>
2024-04-09 21:25:15 +0300

    adding call timeout

--- test/graphql.test.js
+++ test/graphql.test.js
@@ -10,7 +10,7 @@ const apiKey = 'BQYzU9vO6Qn026LTYJHWILo5yv315SE9';
 const authorizationHeader = 'Bearer ory_at_ELdU4qbYct3YrgI52LeLUTeuVQAFkaQUzUMK9iBBMCQ.UMjvcDGwgJMJ8wkJl84Fvr9k6wPRxMNfjeYCa6bQzdU';
 
 describe('GraphQL Queries', function () {
-  it('should return data for Query 1', async function () {
+  it('should return data for Query 1', function (done) {
     const query = gql`
       {
         ethereum(network: ethereum) {
@@ -38,17 +38,21 @@ describe('GraphQL Queries', function () {
       }
     `;
 
-    const data = await request(endpoint, query, null, {
+    request(endpoint, query, null, {
       'x-api-key': apiKey,
       Authorization: authorizationHeader,
+    }).then(data => {
+      console.log(data); // Display the returned data
+
+      // Write assertions to validate the response data
+      chai.expect(data).to.have.property('ethereum');
+      chai.expect(data.ethereum).to.have.property('dexTrades');
+      chai.expect(data.ethereum.dexTrades).to.have.lengthOf.at.least(1);
+      // Add more assertions as needed
+
+      done(); // Call done() to indicate test completion
+    }).catch(error => {
+      done(error); // Call done() with error to indicate test failure
     });
-    console.log(data);
-
-    // Write assertions to validate the response data
-    chai.expect(data).to.have.property('ethereum');
-    chai.expect(data.ethereum).to.have.property('dexTrades');
-    chai.expect(data.ethereum.dexTrades).to.have.lengthOf.at.least(1);
-    // Add more assertions as needed
-  });
-
+  }).timeout(5000); // Set timeout to 5000ms (5 seconds)
 });
