7dde3643f4939f5eb8135084c67c039b777c7fcb
Rani Shaar <rani.shaar@123loadboard.com>
2024-04-10 15:21:38 +0300

    add a new test suite and case

--- test/graphql.test.js
+++ test/graphql.test.js
@@ -2,12 +2,54 @@ let chai;
 before(async () => {
   chai = await import('chai');
 });
+const axios = require('axios');
 
 const { request, gql } = require('graphql-request');
 
 const apiURL = 'https://graphql.bitquery.io';
 const apiKey = 'BQYzU9vO6Qn026LTYJHWILo5yv315SE9';
 const authorizationHeader = 'Bearer ory_at_gwuKBlXBVdL__nEJwRJ1_Npx8b1PUisa4qPuW46bRpk.4S4KyWY3d6-2eDc4p0Co9bkf8b5vhST-zBinxJwvwas';
+
+describe('TST-1000 Invalid Authorization', function () {// Test Suite including all tests related to authorization
+  it('TST-1001 IA Invalid Authorization Key', async function () {// Test case where authorization key in invalid
+    const query = `
+      {
+        ethereum {
+          transactions {
+            count(success: false)
+          }
+        }
+      }
+    `;
+    
+    try {
+      const response = await axios.post(apiURL, { query }, {
+        headers: {
+          'X-API-KEY': apiKey, // set auth key as invalid string
+          'Authorization': authorizationHeader,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      // Extract status and status text from the response object
+      const status = response.status;
+      const statusText = response.statusText;
+
+      // Print status and status text to the console
+      console.log('Response status:', status);
+      console.log('Response status text:', statusText);
+
+      // Assert when status code is not 200 (auth request failed)
+      chai.expect(response.status).equal(200);
+
+    } catch (error) {
+      console.error('Error:', error);
+      throw error; // Rethrow the error to indicate test failure
+    }
+  }).timeout(5000);
+});
+
+
 
 describe('GET ETH data', function () { // Test suite to GET various coin objects
   it('Return ETH failed transactions count', async function () {
@@ -26,8 +68,6 @@ describe('GET ETH data', function () { // Test suite to GET various coin objects
         'X-API-KEY': apiKey,
         'Authorization': authorizationHeader,
       });
-      
-      console.log('ETH failed transactions count:', JSON.stringify(data, null, 2));// Print response call in json format
       
       // Extract count value from the first transaction
       const transactions = data.ethereum.transactions;
