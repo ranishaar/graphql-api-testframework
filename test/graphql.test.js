7d6c19a221d6a0f8b0be0f9952969026120125ac
Rani Shaar <rani.shaar@123loadboard.com>
2024-04-10 11:58:06 +0300

    modify test to properly assert when eth trans count is less than 0 and other changes

--- test/graphql.test.js
+++ test/graphql.test.js
@@ -1,7 +1,7 @@
-/*let chai;
+let chai;
 before(async () => {
   chai = await import('chai');
-});*/
+});
 
 const { request, gql } = require('graphql-request');
 
@@ -9,8 +9,8 @@ const apiURL = 'https://graphql.bitquery.io';
 const apiKey = 'BQYzU9vO6Qn026LTYJHWILo5yv315SE9';
 const authorizationHeader = 'Bearer ory_at_gwuKBlXBVdL__nEJwRJ1_Npx8b1PUisa4qPuW46bRpk.4S4KyWY3d6-2eDc4p0Co9bkf8b5vhST-zBinxJwvwas';
 
-describe('GraphQL Test Cases', function () {
-  it('Return failed ETH transactions count', async function () {
+describe('GET ETH data', function () { // Test suite to GET various coin objects
+  it('Return ETH failed transactions count', async function () {
     const query = gql`
       {
          ethereum {
@@ -27,45 +27,22 @@ describe('GraphQL Test Cases', function () {
         'Authorization': authorizationHeader,
       });
       
-      console.log('Response 1:', JSON.stringify(data, null, 2));
-    } catch (error) {
-      console.error('Error in first request:', error);
-      throw error; // Rethrow the error to indicate test failure
-    }
-
-      // Write assertions to validate the response data
-      //chai.expect(data).to.have.property('ethereum');
-     // chai.expect(data.ethereum).to.have.property('dexTrades');
-     // chai.expect(data.ethereum.dexTrades).to.have.lengthOf.at.least(1);
-      // Add more assertions as needed
-
-
-  }).timeout(5000); // Set timeout to 5000ms (5 seconds)
-
-  it('Return failed ETH transaction count2', async function () {
-    const query = gql`
-      {
-         ethereum {
-             transactions {
-               count(success: false)
-             }
-           }
-      }
-    `;
-
-    try {
-      const data = await request(apiURL, query, null, {
-        'X-API-KEY': apiKey,
-        'Authorization': authorizationHeader,
-      });
+      console.log('ETH failed transactions count:', JSON.stringify(data, null, 2));// Print response call in json format
       
-      console.log('Response 2:', JSON.stringify(data, null, 2));
+      // Extract count value from the first transaction
+      const transactions = data.ethereum.transactions;
+      const count = transactions.length > 0 ? transactions[0].count : undefined;
+
+      console.log('ETH failed transactions count:', count);
+
+      // Assert that count is a number and is greater than 0
+      chai.expect(count).to.exist;
+      chai.expect(count).to.be.a('number');
+      chai.expect(count).to.be.gte(0);
     } catch (error) {
-      console.error('Error in second request:', error);
+      console.error('Error in first request:', JSON.stringify(error, null, 2));
       throw error; // Rethrow the error to indicate test failure
     }
 
-    
-
-  }).timeout(5000); // Set timeout to 5000ms (5 seconds)
+  }).timeout(5000); // Set timeout to 5000ms (5 seconds) because response sometimes was taking more than 2 seconds and failing test
 });
